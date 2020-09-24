7b7264a8f49e7ac64cb07fa6578668d5c34a7bc3
edsolater <edsolater@qq.com>
2020-09-24 22:39:34 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
 function App() {
+  const already = [
+    { height: 200, color: "#233333", position: 0 },
+    { height: 300, color: "#eee", position: 150 },
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className="container">
+        {already.map((block) => (
+          <li
+            className="block"
+            style={{
+              height: block.height,
+              color: block.color,
+              top: block.position,
+            }}
+          ></li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
