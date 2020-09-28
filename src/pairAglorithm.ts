6f0d38deb9b3cd1175224297198f936e3420e674
edsolater <edsolater@qq.com>
2020-09-28 22:51:14 +0800

    同步高亮，已好

--- src/pairAglorithm.ts
+++ src/pairAglorithm.ts
@@ -1,7 +1,4 @@
-export default function pairAglorithm() {
-  const boxA = document.querySelector(".box.A") as HTMLDivElement
-  const boxB = document.querySelector(".box.B") as HTMLDivElement
-
+function pairLocateAglorithm(boxA: HTMLElement, boxB: HTMLElement) {
   boxA.addEventListener("click", () => {
     const rectA = boxA.getBoundingClientRect()
     const rectB = boxB.getBoundingClientRect()
@@ -19,4 +16,29 @@ export default function pairAglorithm() {
       behavior: "smooth",
     })
   })
+}
+function pairHighlightAglorithm(boxA: HTMLElement, boxB: HTMLElement) {
+  boxA.addEventListener("pointerenter", () => {
+    boxA.classList.add("highlighted")
+    boxB.classList.add("highlighted")
+  })
+  boxA.addEventListener("pointerleave", () => {
+    boxA.classList.remove("highlighted")
+    boxB.classList.remove("highlighted")
+  })
+
+  boxB.addEventListener("pointerenter", () => {
+    boxB.classList.add("highlighted")
+    boxA.classList.add("highlighted")
+  })
+  boxB.addEventListener("pointerleave", () => {
+    boxB.classList.remove("highlighted")
+    boxA.classList.remove("highlighted")
+  })
+}
+export default function pairAglorithm() {
+  const boxA = document.querySelector(".box.A") as HTMLDivElement
+  const boxB = document.querySelector(".box.B") as HTMLDivElement
+  pairLocateAglorithm(boxA, boxB)
+  pairHighlightAglorithm(boxA, boxB)
 }
