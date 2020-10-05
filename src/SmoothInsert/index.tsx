5c44df30cb51426dfa74302a9237c1140579f905
zhanggongying <edsolater@qq.com>
2020-10-05 19:33:37 +0800

    尝试失败

--- src/SmoothInsert/index.tsx
+++ src/SmoothInsert/index.tsx
@@ -1,26 +1,59 @@
-import userEvent from "@testing-library/user-event"
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useLayoutEffect, useRef, useState } from "react"
 import "./index.css"
 
+function getTempArray(oldArray: any[], newArray: any[]) {
+  return [null, ...oldArray] //TEMP
+}
+
 export default function SmoothInsert({ array }: { array: any[] }) {
-  const [state, setState] = useState(array)
-  const [toInsert, setToInsert] = useState("")
-  useEffect(() => {
-    document.addEventListener('selectionchange',(e)=>{
-      e.preventDefault()
-    })
-  })
+  const [currentArray, setTargetArray] = useState(array)
+  let oldArr = currentArray
+  let newArr = currentArray
+  const setArray = (inputArray: typeof array) => {
+    oldArr = currentArray
+    newArr = inputArray
+    setTargetArray(inputArray)
+  }
+
+  const screenShotNewItems = () => {}
+  useEffect(() => {}, [currentArray])
+  const liRefs = useRef({})
   return (
     <div className="array-test-box">
-      <div className="data-box">{state.join(" ")}</div>
-      <button className="insert-btn" >+dddddddddd</button>
-      ghjkm, njkmjlkjkl;j
-      <input className="insert-data" value={toInsert} onChange={e=>{
-        console.log(e)
-        document.addEventListener('selectionchange', e => {
-          e.preventDefault()
-        })
-      }}/>
+      <button
+        className="insert-btn"
+        onClick={() => {
+          setArray([...currentArray, new Date().getSeconds()])
+        }}
+      >
+        +
+      </button>
+      <ul className="array-box">
+        {currentArray.map((item) => (
+          <li
+            ref={(el) => {
+              liRefs.current[item] = el
+            }}
+            className={`array-item ${true ? "enter-active" : ""}`}
+            key={item}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+      {/* <ul className="shadow-array-box">
+        {currentArray.map((item) => (
+          <li
+            ref={(el) => {
+              liRefs.current[item] = el
+            }}
+            className={`array-item ${intransition ? "enter-active" : ""}`}
+            key={item}
+          >
+            {item}
+          </li>
+        ))}
+      </ul> */}
     </div>
   )
 }
